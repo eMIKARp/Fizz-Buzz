b51e8e28d1852b9cfcc6f1dc90099d7c0dc44fc4
Karpowicz <Emil.Karpowicz@unilever.com>
2018-03-05 17:02:06 +0100

    Initial commit

---
+++ src/pkg004/fizzbuzz/Main.java
@@ -0,0 +1,15 @@
+package pkg004.fizzbuzz;
+
+/**
+ * Pro/g/ramming Challenges v 3.0
+ * Challenge 004: FizzBuzz
+ * @author Emil.Karpowicz
+ */
+public class Main {
+
+    public static void main(String[] args) 
+    {
+
+    }
+    
+}
